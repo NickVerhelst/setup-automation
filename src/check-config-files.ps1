53a7f0919b908f3410914e860ec50e488cd8a871
Nick Verhelst <nick.verhelst@dexmach.com>
2021-12-30 10:13:13 +0100

    Added script to read a config file of packages and filter out the comments

--- src/check-config-files.ps1
+++ src/check-config-files.ps1
@@ -1,2 +1,10 @@
 # Check if there are any duplicate packages
 # Report on duplicates in shell
+$fileLocation = "Location of file to check"
+$fileName = "name"
+$outputLocation = "./logs/$fileName"
+
+foreach($line in Get-Content $fileToCheck) {
+    Write-Output
+}
+
