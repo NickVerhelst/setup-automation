3618eac33cc79e2aebe8c3a46568f45232fbb4ed
Nick Verhelst <nick.verhelst@dexmach.com>
2021-12-30 10:49:46 +0100

    Upgraded "read configs" to a PowerShell module + added test script

---
+++ src/read-configs.psm1
@@ -0,0 +1,40 @@
+<# READ CONFIGS
+    This module file contains all required functions for reading in the config files.
+#>
+function Read-ConfigFile{
+    param(
+        [string[]] $pathToFile = './config/',
+        [string[]] $fileName,
+        [string[]] $pathToLog = './logs/',
+        [string[]] $commentSymbol = '#',
+        [string[]] $packagesSeparator = ' '
+    )
+    $today = (Get-Date -Format "yyyyMMdd")
+    $logName = "$($today)$($fileName)"
+    $fullPathToFile = (Join-Path "$($pathToFile)" "$($fileName)")
+    $fullPathToLog = (Join-Path "$($pathToLog)" "$($logName)")
+    foreach ($line in Get-Content -Path $fullPathToFile) {
+        $lineToAdd = $line
+        # We remove blank lines (can be placeholders)
+        $keepLine = $lineToAdd.trim() -ne ""
+        # We remove comment lines (starting with comment or blank spece)
+        $lineToAdd = $lineToAdd | Select-String -Pattern "^[$($commentSymbol)]" -NotMatch
+        # We also remove comments AFTER a package by looking for the comment symbol
+        $positionOfComment = "$($lineToAdd)".IndexOf("$($commentSymbol)")
+        if ($positionOfComment -gt 0) {
+            $lineToAdd = "$($lineToAdd)".Substring(0, $positionOfComment)
+        }
+        # Finaly we trim all whitespaces and concatenate the different packages
+        if ($lineToAdd.length -gt 0) {
+            $lineToAdd = "$($lineToAdd)".trim()
+            $lineToAdd = "$($lineToAdd)$($packagesSeparator)"
+        }
+        if ($keepLine) {
+        $lines += $lineToAdd
+        }
+    }
+    # At the end we clean up the trailing comma from the last package.
+    return $lines.Substring(0, $lines.length-2)
+}
+# At the end of the module we explicitly expose our function to the end users.
+Export-ModuleMember -Function Read-ConfigFile
