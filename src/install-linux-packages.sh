8a055fa2641807d0fc912b0f6a4599cc34a49352
Nick Verhelst <nick.verhelst@dexmach.com>
2022-01-01 19:12:11 +0100

    First setup for Linux functionalities

--- src/install-linux-packages.sh
+++ src/install-linux-packages.sh
@@ -1,1 +1,15 @@
-# Loop trough config file ... .
+#!/bin/bash
+parent_path=$(pwd)
+relative_path="testfiles/test-linux-packages.txt"
+full_path="$parent_path/$relative_path"
+# Convert input text file to Unix format (needed for correct line endings)
+dos2unix $full_path
+# Convert file to remove all comments
+# $file_without_comments="$("${full_path}" | grep -v '^#' | grep -v '^\s*$' | cut -d ' ' -f1)"
+while IFS= read -r line; do
+    $line | cut -d '#' -f1 > echo
+    # echo '%s\n' "Installing $parsed_line"
+    # sudo apt-get install $line | xargs
+done < $full_path
+
+# sudo apt-get install <package>
--- src/update-linux-distro.sh
+++ src/update-linux-distro.sh
@@ -1,2 +1,4 @@
 # Ensure Linux system is fully updated
 sudo apt update $$ sudo apt upgrade -y
+# Install dos2unix (needed to read txt file to have correct line endings)
+sudo apt install dos2unix
