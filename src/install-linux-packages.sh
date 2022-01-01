cde2eccd2f95754c375ea531c6198301e3945aa4
Nick Verhelst <nick.verhelst@dexmach.com>
2022-01-01 20:23:17 +0100

    Further bash experimentation to parse file with comments.

--- src/install-linux-packages.sh
+++ src/install-linux-packages.sh
@@ -4,12 +4,13 @@ relative_path="testfiles/test-linux-packages.txt"
 full_path="$parent_path/$relative_path"
 # Convert input text file to Unix format (needed for correct line endings)
 dos2unix $full_path
+# Create temp file
+parsed_file='$($full_path) | grep -v "^#" | grep -v "^\s*$"'
 # Convert file to remove all comments
 # $file_without_comments="$("${full_path}" | grep -v '^#' | grep -v '^\s*$' | cut -d ' ' -f1)"
 while IFS= read -r line; do
-    $line | cut -d '#' -f1 > echo
-    # echo '%s\n' "Installing $parsed_line"
+#     $line | cut -d '#' -f1 > echo
+    echo '%s\n' "Installing $parsed_line"
     # sudo apt-get install $line | xargs
-done < $full_path
-
-# sudo apt-get install <package>
+    # sudo apt-get install <package>
+done < "$parsed_file"
